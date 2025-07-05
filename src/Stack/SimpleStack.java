fa6bd9237a8d862f06d08864834e2e39bef07754
Dine256 <kjdinesh29@gmail.com>
2025-07-05 12:15:38 +0530

    Initial commit

---
+++ src/Stack/SimpleStack.java
@@ -0,0 +1,18 @@
+package Stack;
+
+import java.util.Stack;
+
+public class SimpleStack {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+        stack.push("Dinesh");
+        stack.push("Vishala");
+        stack.push("Bhuvana");
+        stack.push("Lakshu");
+        stack.push("Cin");
+
+
+        System.out.println(stack.search("Bhuvana"));
+        System.out.println(stack);
+    }
+}
